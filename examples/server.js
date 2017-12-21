28b32b6dee665852d74e672179eb6cae9509b21d
YingshanDeng <yingshandeng@gmail.com>
2017-12-21 16:42:04 +0800

    edit  plain text collaboratively (add/delete text and new line)

--- examples/server.js
+++ examples/server.js
@@ -8,15 +8,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.use(express.static('dist'))
-app.use(express.static('ot.js'))
+app.use(express.static('build'))
+app.use(express.static('lib'))
 app.use(express.static('node_modules'))
 
 http.listen(3000, function () {
   console.log('listening on *:3000')
 })
 
-var EditorSocketIOServer = require('./ot.js/editor-socketio-server.js')
+var EditorSocketIOServer = require('../build/SharedPenServer.js')
 var server = new EditorSocketIOServer('', [], 1)
 
 io.on('connection', function (socket) {
